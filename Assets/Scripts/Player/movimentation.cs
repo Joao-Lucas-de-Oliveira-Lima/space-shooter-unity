db1444119e9d0acff3fe615603ec1d4e2dc353ed
Joao-Lucas-de-Oliveira-Lima <63080135+Joao-Lucas-de-Oliveira-Lima@users.noreply.github.com>
2023-05-31 15:15:05 -0300

    Adding colliders to the sides of the screen

--- Assets/Scripts/Player/movimentation.cs
+++ Assets/Scripts/Player/movimentation.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class movimentation : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float speed;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
     void Update()
     {
-        
+        float xDirection = Input.GetAxisRaw("Horizontal");
+        float yDirection = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(xDirection, yDirection).normalized * speed;
     }
 }
