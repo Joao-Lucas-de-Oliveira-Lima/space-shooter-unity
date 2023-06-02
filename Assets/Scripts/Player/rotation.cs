73b3d9ca4ae861f2d4af42093db18ab68d52251b
Joao-Lucas-de-Oliveira-Lima <63080135+Joao-Lucas-de-Oliveira-Lima@users.noreply.github.com>
2023-06-02 11:45:24 -0300

    Improve player rotation

--- Assets/Scripts/Player/rotation.cs
+++ Assets/Scripts/Player/rotation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class rotation : MonoBehaviour
 {
+    public float speed;
     void Start()
     {
 
@@ -14,5 +15,12 @@ public class rotation : MonoBehaviour
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         Vector2 direction = new Vector2(mousePosition.x - this.transform.position.x, mousePosition.y - this.transform.position.y);
         transform.up = direction;
+        /*
+        Vector3 difference = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.transform.position).normalized;
+        float angle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(0f, 0f, angle), speed * Time.deltaTime);
+        //Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
+        this.transform.rotation = rotation;
+        */
     }
 }
